dbb0b6007873ca0f9c68504f51784a4c58b5e0b0
rahulkumar <rahulkr009@gmail.com>
2026-02-25 01:17:02 +0530

    BE resources update

--- PAGINATION_EXAMPLE/FRONTEND_REACT/src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import paginationPostsSlice from "./paginationPostSlice";
-
-export const store = configureStore({
-  reducer: {
-    paginationPosts: paginationPostsSlice,
-  },
-});
